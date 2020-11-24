fd0520b7874314a32fd28a8310ab4e2cd7184b7e
EVinteler <ericavinteler@yahoo.com>
2020-11-24 12:17:13 +0200

    finished step 21 successfully

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Vinteler_Erica_lab8.Models
 {
     public class Book
     {
         public int ID { get; set; }
+        
+        [Display(Name = "Book Title")]
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
         public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
 
     }
 }
